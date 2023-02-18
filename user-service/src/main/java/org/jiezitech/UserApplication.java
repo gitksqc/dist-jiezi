50dbd81522e19dc76739a79f5ad8b255c2ea3028
sushun <781836670@qq.com>
2023-02-18 17:12:32 +0800

    添加user和common模块

---
+++ user-service/src/main/java/org/jiezitech/UserApplication.java
@@ -0,0 +1,17 @@
+package org.jiezitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SuShun
+ * @description TODO
+ * @date 2023/2/18 12:50
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
